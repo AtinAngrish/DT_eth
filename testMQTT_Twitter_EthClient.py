7d20a88045fc82364c0ae8bc4826908f68117cbb
Beniamjn <37417196+Beniamjn@users.noreply.github.com>
2018-03-19 15:40:37 -0400

    Add files via upload

--- testMQTT_Twitter_EthClient.py
+++ testMQTT_Twitter_EthClient.py
@@ -31,53 +31,48 @@ class stateWatcher:
         self.machine1_mac = "AAA"
         self.machine1_time = 3
         self.machine1_rate = 1
-#        self.CH.addUser()
-#        self.CH.addMachine(self.machine1_name, self.machine1_mac, self.machine1_time, self.machine1_rate)
-        print(self.CH.getMachineInfo(self.CH.mining_account,0))
+        #self.CH.addUser()
+        #self.CH.addMachine(self.machine1_name, self.machine1_mac, self.machine1_time, self.machine1_rate)
+        #print(self.CH.getMachineInfo(self.CH.mining_account,0))
     
     def set_value(self, new_value):
         start = time.time()
         if self.variable != new_value:
             self.old = self.variable
-            self.variable =new_value
+            self.variable = new_value
             self.ch_variable = self.old
             delta = time.time() - start
             self.on_change(delta)
         else:
+            #print('else')
             pass
               
-    def on_change(self,delta):
-        # do stuff when variable changed
+    def on_change(self,delta): # do stuff when variable changed
+        print('changed')
         if self.variable=="idle":
             self.counter+=1 
             # We are updating only the state of the machine on the chain for now
-            self.CH.updateMachineStatus(self.CH.mining_account, 
+            '''self.CH.updateMachineStatus(self.CH.mining_account, 
                                         self.machine1_mac, 
                                         True, 
                                         self.machine1_time,
-                                        self.machine1_rate)
-            #while not self.CH.getMachineInfo(self.machine1_mac, 0):
-            #    pass #this will stall the program until the transaction is recorded in a block
+                                        self.machine1_rate)'''
             tweet = "Update# "+str(self.counter) + " : I am now available!"
             print(tweet)
-            api.update_status(status=tweet)
-        elif self.variable=="cutting":
+            #api.update_status(status=tweet)
+        elif self.variable == "cutting":
             self.counter += 1
-            self.CH.updateMachineStatus(self.CH.mining_account
+            '''self.CH.updateMachineStatus(self.CH.mining_account
                                         , self.machine1_mac
                                         , False
                                         , self.machine1_time
-                                        , self.machine1_rate)
-            #while self.CH.getMachineInfo(self.machine1_mac, 0):
-            #    pass #this will stall the program until the transaction is recorded in a block
+                                        , self.machine1_rate)'''
             tweet = "Update# "+str(self.counter) + " : I am now busy executing order from "+self.CH.trading_account
             print(tweet)
-            api.update_status(status=tweet)
+            #api.update_status(status=tweet)
 
-        #tweet = "I am now "+self.variable
-
-Broker="iot.eclipse.org"
-sub_topic = "LCNC/data"       # send messages to this topic
+Broker = "iot.eclipse.org"
+sub_topic = "LCNC/data" # send messages to this topic
 pub_topic = "LCNC/commands"
 state = stateWatcher('idle')
 # when connecting to mqtt do this;
@@ -85,25 +80,22 @@ prev = "idle"
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
     client.subscribe(sub_topic)
-# when receiving a mqtt message do this;
 
 def on_message(client, userdata, msg):
-    message = str(msg.payload)
-    #print(msg.topic+" "+message)
-    mach_status=str(message)
-    #print(mach_status)
+    print('on message')
+    current_line, mach_status = msg.payload.decode().replace('(', '').replace(')', '').split(', ')
+    mach_status = str(mach_status.replace("'", ""))
     state.set_value(mach_status)
-    print(mach_status)
+
 
 def on_publish(mosq, obj, mid):
     print("mid: " + str(mid))
+
 while True:
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    
     client.connect(Broker, 1883, 60)
-    
     client.loop_forever()
